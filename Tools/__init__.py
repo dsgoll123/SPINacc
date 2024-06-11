4e43a1d3dfa3d7f834de53628bc698c1b3641dad
tztsai <1213815284@qq.com>
2024-06-11 12:39:20 +0200

    let ruff fix some issues

--- Tools/__init__.py
+++ Tools/__init__.py
@@ -22,19 +22,15 @@ import numpy as np
 import pandas as pd
 from netCDF4 import Dataset
 import calendar
-import matplotlib
 
 matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
 import random
 import json
-import random
 import sys
 from collections import Counter
 
-import matplotlib.colors as mcolors
-import matplotlib.pyplot as plt
 from imblearn.over_sampling import SMOTE
 from mpl_toolkits.basemap import Basemap
 from pandas import DataFrame, Series
@@ -42,9 +38,6 @@ from scipy import stats
 from sklearn.cluster import Birch, KMeans
 from sklearn.datasets import make_regression
 from sklearn.ensemble import BaggingRegressor
-from imblearn.over_sampling import SMOTE
-from collections import Counter
-from scipy import stats
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.model_selection import LeaveOneOut
 from sklearn.tree import DecisionTreeRegressor
